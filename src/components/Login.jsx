5905fd5b04fa02a33f50028a662e9193540b1006
Ammar <ammar@pop-os.localdomain>
2020-12-29 21:29:04 +0300

    😎 Add material UI handle error

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,21 +2,12 @@ import img from "../img/sign.svg";
 import "../Style/login.css";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-// import Snackbar from "@material-ui/core/Snackbar";
-// import MuiAlert from "@material-ui/lab/Alert";
-// import { makeStyles } from "@material-ui/core/styles";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
 
-// function Alert(props) {
-//   return <MuiAlert elevation={6} variant="filled" {...props} />;
-// }
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     width: "100%",
-//     "& > * + *": {
-//       marginTop: theme.spacing(2),
-//     },
-//   },
-// }));
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 function Login() {
   useEffect(() => {
     document.title = "Sign In";
@@ -24,18 +15,20 @@ function Login() {
 
   const [phone, setPhone] = useState("");
   const [password, setPassword] = useState("");
-  // const [open, setOpen] = React.useState(false);
-  // const handleClose = (event, reason) => {
-  //   if (reason === "clickaway") {
-  //     return;
-  //   }
-  //   setOpen(false);
-  // };
+  const [open, setOpen] = React.useState(false);
+  const [msg, setMsg] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
   const changePhone = (e) => setPhone(e.target.value);
   const changePassword = (e) => setPassword(e.target.value);
   function handlSubmit(e) {
     e.preventDefault();
-
+    setIsLoading(true);
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
@@ -51,20 +44,28 @@ function Login() {
       .then((response) => response.json())
       .then((result) => {
         if (!result.status) {
-          return alert(result.err);
-          //TODO: add beuty alert from matirial UI
-          // setOpen(true);
-          // console.log(open);
-          // <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-          //   <Alert severity="error">This is an error message!</Alert>
-          // </Snackbar>;
-        }
-        return alert(result.status);
+          setOpen(true);
+          if (typeof result.err === "object") {
+            setOpen(true);
+            Object.keys(result.err).forEach((key) => {
+              setMsg(result.err[key]);
+            });
+          } else setMsg(result.err);
+        } else setOpen(false);
       })
       .catch((error) => alert(error));
+    setIsLoading(false);
   }
+
   return (
     <>
+      {open ? (
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert severity="error"> {msg}</Alert>
+        </Snackbar>
+      ) : (
+        console.log("correct info!")
+      )}
       <div className="continer">
         <h1>Get Started</h1>
         <form className="my-form" onSubmit={handlSubmit}>
@@ -82,9 +83,13 @@ function Login() {
             <input type="password" name="password" onChange={changePassword} />
           </div>
           <input className="sign" type="submit" value="Sign In" />
-          <br/>
+          <br />
           <Link to="/singup">
-            <input className="signup" type="button" value="Create New Account" />
+            <input
+              className="signup"
+              type="button"
+              value="Create New Account"
+            />
           </Link>
         </form>
       </div>
